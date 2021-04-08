c0a6f489c9cdc2103675aebb8171200be348edca
prachal80 <prachaljpatel@gmail.com>
2021-04-07 23:31:28 -0700

    Memcached configured on local

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const bodyParser = require('body-parser');
 const getProductDetails = require('./Backend/src/routes/getProductDetails');
 const postProduct = require("./Backend/src/routes/postProduct");
 const app = express();
+var Memcached = require('memcached');
+
+// Created object for memcached
+var memcached = new Memcached();
+/* code to connect with your memecahced server */
+memcached.connect( 'localhost:11211', function( err, conn ){
+    console.log("connected with memccached");
+if( err ) {
+console.log( conn.server,'error while memcached connection!!');
+}
+});
 
 mongoose.connect(mongoDB, (err) => {
     if (err) {
@@ -41,9 +52,21 @@ app.get('/', function(res, req){
     res.send("Hello World!");
 });
 
-app.get('/product/:product_id', (req, res) => {
+app.get('/product/:_id', (req, res) => {
 
-    getProductDetails(req.params.product_id)
+    getProductDetails(req.params._id)
+    .then(result => {
+
+        res.status(200).send(result);
+    })
+    .catch(err => {
+        res.status(500).send(err);
+    })
+});
+
+app.get('/product/:product_name', (req, res) => {
+
+    getProductDetails(req.params.product_name)
     .then(result => {
 
         res.status(200).send(result);
@@ -55,14 +78,34 @@ app.get('/product/:product_id', (req, res) => {
 
 app.get('/products/all', (req, res) => {
 
-    Products.find()
-    .then(result => {
+    const searchTerm = 'all';
+    // method to get saved data....
+    memcached.get(searchTerm, function (err, result) {
+        
+        if(result != null){
+            //console.log("*********** Data from cache found ***********");
+            res.status(200).send( JSON.parse( result ));
+        }
+        else{
+            Products.find()
+            .then(result1 => {
+                console.log("DB Read performed");
 
-        res.status(200).send(result);
-    })
-    .catch(err => {
-        res.status(500).send(err);
-    })
+                // saving information to user key.
+                memcached.set(searchTerm, JSON.stringify(result1), 10000, function (err1) {
+                if(err1) throw new err1;
+                res.status(200).send(result1);
+        
+                
+            });
+             
+            })
+            .catch(err2 => {
+                res.status(500).send(err2);
+            })
+        }
+        });
+
 });
 
 app.post('/add/product', (req, res) => {
